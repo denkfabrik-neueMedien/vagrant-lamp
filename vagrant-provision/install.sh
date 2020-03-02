233be4639793a5a1d7d0a3d94cc99ec5caefd27c
Marco Kernler <marco.kernler@denkfabrik-neueMedien.de>
2020-03-02 14:30:48 +0100

    PHP update to v7.1

--- vagrant-provision/install.sh
+++ vagrant-provision/install.sh
@@ -22,22 +22,22 @@ sudo service mysql restart
 # php
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y libapache2-mod-php7.0 \
-                        php7.0 \
-                        php7.0-cli \
-                        php7.0-curl \
-                        php7.0-gd \
-                        php7.0-imap \
-                        php7.0-intl \
-                        php7.0-mbstring \
-                        php7.0-mcrypt \
-                        php7.0-mysql \
-                        php7.0-tidy \
-                        php7.0-xml \
-                        php7.0-xsl \
-                        php7.0-zip \
-                        php7.0-opcache \
-                        php7.0-xdebug \
+sudo apt-get install -y libapache2-mod-php7.1 \
+                        php7.1 \
+                        php7.1-cli \
+                        php7.1-curl \
+                        php7.1-gd \
+                        php7.1-imap \
+                        php7.1-intl \
+                        php7.1-mbstring \
+                        php7.1-mcrypt \
+                        php7.1-mysql \
+                        php7.1-tidy \
+                        php7.1-xml \
+                        php7.1-xsl \
+                        php7.1-zip \
+                        php7.1-opcache \
+                        php7.1-xdebug \
                         php-imagick \
                         php-oauth \
                         php-pear
